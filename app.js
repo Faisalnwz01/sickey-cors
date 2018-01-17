9aa8c6b8ce788b6e2dd9e738586b7bb2758586f0
Faisal Nawaz <faisalnawaz@NBCMacbook-0216.home>
2018-01-17 15:47:14 -0500

    push updates

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 
-app.configure(function() {
-  // Set the IP and port to use the OpenShift variables.
-  app.set('port', process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080);
-  app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0');
-});
-
+var port =  process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
+var host = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0'
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: ['https://sickey.netlify.com', 'https://www.sickey.io', 'https://sickey.io', 'http://localhost:4200'], // Allow all origins
