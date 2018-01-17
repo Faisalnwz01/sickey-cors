3e89d58b671dbac2ba3e968dc2c59d25033a232b
Faisal Nawaz <faisalnawaz@NBCMacbook-0216.home>
2018-01-17 18:10:52 -0500

    update '' to whitelist

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port =  process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL
 var host = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0'
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ['https://sickey.netlify.com', 'https://www.sickey.io', 'https://sickey.io', 'http://localhost:4200'], // Allow all origins
+    originWhitelist: ['https://sickey.netlify.com', 'https://www.sickey.io', 'https://sickey.io', 'http://localhost:4200', ''], // Allow all origins
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
