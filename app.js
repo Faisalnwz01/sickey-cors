b587ac2c2cb4c71b213af1b07c6a43d87c759cbb
Faisal nawaz <fslnwz0@gmail.com>
2018-05-26 14:45:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ var port =  process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL
 var host = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0'
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ['https://sickey.netlify.com', 'https://www.sickey.io', 'https://www.dev.sickey.io', 'https://sickey.io', 'https://dev.sickey.io', 'http://localhost:4200', 'http://localhost:8100', ''], // Allow all origins
+    originWhitelist: [
+        'https://sickey.netlify.com',
+        'https://www.sickey.io',
+        'https://www.dev.sickey.io',
+        'https://sickey.io',
+        'https://dev.sickey.io',
+        'http://localhost:4200',
+        'http://localhost:8100',
+        '', 
+        'https://akount.netlify.com',
+        'https://*.akount.netlify.com'], // Allow all origins
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
